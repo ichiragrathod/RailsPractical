bbd42d428b8070f8c481e1bc1e48956a6a4183e7
chirag <chirag.r@simformsolutions.com>
2022-03-13 10:35:49 +0530

    Added validations for Author.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+    VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true,uniqueness:{case_sensitive:false}, 
+    length: {minimum:3, maximum:105},
+    format:{ with:VALID_EMAIL_REGEX }
+
+    validates :first_name, presence: true, length:{minimum:2, maximum:100}
+    validates :last_name, presence: true, length:{minimum:2, maximum:100}
+    validates :date_of_birth, presence: true, length:{minimum:2, maximum:100}
 end

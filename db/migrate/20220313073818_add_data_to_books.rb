020029389fb530ae1b729c517b98fb8c0f6ce978
chirag <chirag.r@simformsolutions.com>
2022-03-14 11:48:54 +0530

    Created migration with up/down and reversible in add data.

--- db/migrate/20220313073818_add_data_to_books.rb
+++ db/migrate/20220313073818_add_data_to_books.rb
@@ -1,10 +1,25 @@
 class AddDataToBooks < ActiveRecord::Migration[7.0]
   def change
-    Book.create(name: "Ruby on Rails Tutorial: Learn Web Development ",price: "999.0", author_id: 1)
-    Book.create(name: "Introduction to Algorithms",price: "1056.0", author_id: 1)
-    Book.create(name: "The Pragmatic Programmer",price: "750.0", author_id: 2)
-    Book.create(name: "Agile Web Development with Rails 6",price: "600.0", author_id: 2)
-    Book.create(name: "Basic Of Computer Hardware and Software",price: "560.0", author_id: 3)
-    Book.create(name: "Programming With Ruby",price: "800.0", author_id: 3)
+    reversible do |dir|
+      dir.up do
+        Book.create(name: "Ruby on Rails Tutorial: Learn Web Development ",price: "999.0", author_id: 1)
+        Book.create(name: "Introduction to Algorithms",price: "1056.0", author_id: 1)
+        Book.create(name: "The Pragmatic Programmer",price: "750.0", author_id: 2)
+        Book.create(name: "Agile Web Development with Rails 6",price: "600.0", author_id: 2)
+        Book.create(name: "Basic Of Computer Hardware and Software",price: "560.0", author_id: 3)
+        Book.create(name: "Programming With Ruby",price: "800.0", author_id: 3)
+      end
+
+      dir.down do
+        Book.find_by(name:"Ruby on Rails Tutorial: Learn Web Development ").destroy
+        Book.find_by(name:"Introduction to Algorithms").destroy
+        Book.find_by(name:"The Pragmatic Programmer").destroy
+        Book.find_by(name:"Agile Web Development with Rails 6").destroy
+        Book.find_by(name:"Basic Of Computer Hardware and Software").destroy
+        Book.find_by(name:"Programming With Ruby").destroy
+      end
+
+    end
+
   end
 end

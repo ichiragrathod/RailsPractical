6e98fd33a625a2cfcda5308b74372d5507811c71
chirag <chirag.r@simformsolutions.com>
2022-03-21 09:01:57 +0530

    Callbacks Added in Student and Faculty Model.

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -18,4 +18,47 @@ class Faculty < ApplicationRecord
                 :inclusion => { :in => %w(Ass.Prof Prof),:message => "can't be %{value}" },
                 :exclusion => { :in => %w(HOD Sr.Prof.),:message => "%{value} is reserved." }
 
+    
+    #Callbacks code here...
+        
+    before_validation :normalize_name, :remove_whitespaces
+
+    after_initialize do |faculty|
+        puts "You have initialized an faculty!"
+    end
+
+    after_find do |faculty|
+        puts "You have found an faculty!"
+    end
+
+    after_create do |faculty|
+        puts "Faculty #{faculty.first_name} #{faculty.last_name} has been created."
+    end
+
+    after_update do |faculty|
+        puts "Faculty #{faculty.first_name} #{faculty.last_name} has been updated."
+    end
+
+    after_destroy do |faculty|
+        puts "Faculty #{faculty.first_name} #{faculty.last_name} has been destroyed."
+    end
+
+    after_save do |faculty|
+        puts "Email Validated"
+    end
+
+    after_validation :check_date_of_birth, if: Proc.new { |faculty| faculty.errors[:date_of_birth] == [] }
+    def check_date_of_birth
+        puts "Faculty Date of birth is Vaild."
+    end
+
+    def remove_whitespaces
+        self.first_name.strip!
+        self.last_name.strip!
+    end
+
+    def normalize_name
+        self.first_name = first_name.downcase.titleize
+        self.last_name = last_name.downcase.titleize
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -12,4 +12,50 @@ class Student < ApplicationRecord
     validates :department, :inclusion => { :in => %w(IT CE),
         :message => "can't be %{value}" }
 
+    
+    #Callbacks code here...
+    
+    before_validation :normalize_name, :remove_whitespaces
+
+    after_initialize do |student|
+        puts "You have initialized an student!"
+    end
+
+    after_find do |student|
+        puts "You have found an student!"
+    end
+
+    after_create do |student|
+        puts "Student #{student.first_name} #{student.last_name} has been created."
+    end
+    
+    after_update do |student|
+        puts "Student #{student.first_name} #{student.last_name} has been updated."
+    end
+
+    after_destroy do |student|
+        puts "Student #{student.first_name} #{student.last_name} has been destroyed."
+    end
+
+    before_validation :check_date_of_birth, if: Proc.new { |student| student.errors[:date_of_birth] == []}
+
+    def check_date_of_birth
+        puts "Student Date of birth is Vaild."
+    end
+
+    after_destroy :not_allowed_name, if: Proc.new { |student| student.first_name.downcase == "abc" or student.first_name.downcase == "xyz" or student.first_name.downcase == "temp"}
+    
+    def not_allowed_name
+        puts "#{self.first_name} is deleted, beacuse it's not allowed!"
+    end
+
+    def remove_whitespaces
+        self.first_name.strip!
+        self.last_name.strip!
+    end
+
+    def normalize_name
+        self.first_name = first_name.downcase.titleize
+        self.last_name = last_name.downcase.titleize
+    end
 end

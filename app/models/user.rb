ef9d5be3c1eda83611e56ce3b91e0626be088bd6
chirag <chirag.r@simformsolutions.com>
2022-04-07 13:37:12 +0530

    Added addresses tabel.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_secure_password
 
   has_and_belongs_to_many :events
-  
+  has_one :address, :as => :addressable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_092309) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_080038) do
+  create_table "addresses", force: :cascade do |t|
+    t.string "user_address"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -126,4 +134,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_092309) do
     t.string "password_digest"
   end
 
+  add_foreign_key "addresses", "users"
 end

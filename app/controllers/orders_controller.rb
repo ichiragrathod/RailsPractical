d92d500bf1d5bce055c814e3644d62aaba96561e
chirag <chirag.r@simformsolutions.com>
2022-04-18 12:26:40 +0530

    Added Admin and Marchant Authentication.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-    
 	before_action :set_order, only: [:show, :edit, :update, :destroy]
-
+	before_action :require_admin, except:[:index]
+	
 	def index
 		@orders = Order.all
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable  
+  enum role: [:merchant, :admin]
+
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
 end

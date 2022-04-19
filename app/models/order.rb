cb476d982371ff4af78260fce6cd95d01f9ba94a
chirag <chirag.r@simformsolutions.com>
2022-04-19 10:00:54 +0530

    Formatted the code

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :product
-
   private
     before_create :calculate_total_price
-    
     def calculate_total_price
       product_price = Product.where('id = ?', product_id).pluck(:price)[0]
       self.total_price = product_price * quantity
